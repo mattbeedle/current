8c8d9d65c36ddb7dc9b4ba706492304489c1cf7f
Matt Beedle <matt.beedle@1000jobboersen.de>
2010-10-31 19:58:14 +0100

    Alias old methods for compatibility. Last set of changes broke a bunch of our sites

--- lib/current.rb
+++ lib/current.rb
@@ -3,28 +3,34 @@ module Current
   def controller_is?(*attrs)
     attrs.collect{|attr| attr.to_s}.include?(controller_name)
   end
+  alias :controller_is :controller_is?
 
   def action_is?(*attrs)
     attrs.map{|attr| attr.to_s}.include?(action_name)
   end
+  alias :action_is :action_is?
 
   def partial_is?(param)
     param == params[:partial]
   end
+  alias :partial_is :partial_is?
 
   def controller_action_is?(c,a)
     controller_is(c) && action_is(a)
   end
+  alias :controller_action_is :controller_action_is?
 
   def active_if?(condition)
     condition ? "active" : "inactive"
-  end                         
+  end
+  alias :active_if :active_if?
 
   def nav_link_to(text,path,condition, options={})
     klass = active_if?(condition) + " #{options[:class]}"
     linktext = condition ? "#{text}" : text
     link_to(raw(linktext), path, options.merge(:class => klass.strip))
   end
+  alias :nav_link_to :nav_link_to?
 
 end
 
